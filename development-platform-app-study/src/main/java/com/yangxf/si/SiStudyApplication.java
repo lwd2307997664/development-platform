1733df48685435637d949c71b119858f55d47d26
lin.wd <lin.wd@neusoft.com>
2020-03-06 23:36:08 +0800

    初始化

---
+++ development-platform-app-study/src/main/java/com/yangxf/si/SiStudyApplication.java
@@ -0,0 +1,22 @@
+/**
+ * FileName: SiStudyApplication
+ * Author:   Administrator
+ * Date:     2020/3/6 22:44
+ * Description: 学习应用启动文件
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.yangxf.si;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈学习应用启动文件〉
+ *
+ * @author Administrator
+ * @create 2020/3/6
+ * @since 1.0.0
+ */
+public class SiStudyApplication {
+
+}
